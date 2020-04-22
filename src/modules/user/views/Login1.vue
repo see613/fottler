f357a3e1938a90ddca29ec05fa1ce733342ec359
see613 <see613@mail.ru>
2020-04-22 16:45:02 +0300

    test link

--- src/modules/user/views/Login1.vue
+++ src/modules/user/views/Login1.vue
@@ -46,6 +46,10 @@
                     <error :text="errors.vk"
                            class="center"></error>
                 </div>
+
+                <div class="mb-15">
+                    <a href="#" @click.prevent="test">GET-запрос</a>
+                </div>
             </td>
         </tr>
     </table>
@@ -58,6 +62,8 @@
     import {afterAuth, submitLogin1, vkAuth} from "@/modules/user/store/auth/auth.types";
     import MenuSwitcher from "@/components/menu/MenuSwitcher";
     import VkAuth from "@/components/VkAuth";
+    import ServerRequest from "@/core/ServerRequest";
+    import ApiConfig from "@/config/ApiConfig";
 
     const { mapState, mapActions } = createNamespacedHelpers('auth');
     const { mapFields } = createHelpers({
@@ -73,6 +79,13 @@
             ...mapFields(['email'])
         },
         methods: {
+
+            async test(){
+                await ServerRequest.get(ApiConfig.urls.user.vkAuth, {
+                    test: 'test'
+                });
+            },
+
             async onSubmit(){
                 await this[submitLogin1]();
             },
